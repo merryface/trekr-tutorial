7ecbb42ec340b3514840716054f451244a376621
Tarik <tarikmerrylees@gmail.com>
2022-07-18 23:48:53 +0100

    create map page

---
+++ Shared/WorldView.swift
@@ -0,0 +1,33 @@
+//
+//  WorldView.swift
+//  Trekr
+//
+//  Created by Tarik Merrylees on 18/07/2022.
+//
+
+import MapKit
+import SwiftUI
+
+struct WorldView: View {
+    @State var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 51.507222,
+            longitude: -0.1275
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 40,
+            longitudeDelta: 40
+        )
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .navigationTitle("Locations")
+    }
+}
+
+struct WorldView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldView()
+    }
+}
