4e546265528973a8b9670d48d0978e6152a2d18e
Tarik <tarikmerrylees@gmail.com>
2022-07-18 20:17:48 +0100

    Initial Commit

---
+++ Shared/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Shared
+//
+//  Created by Tarik Merrylees on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
